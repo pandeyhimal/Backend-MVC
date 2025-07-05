db5949ea75366cca9f58a425da6ddf59f78a6a8b
itsm22051@gmail.com <himalpandey2002@gmail.com>
2025-07-05 23:30:08 +0545

    Routes

---
+++ routes/userroutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const {userInsert, userRead, userUpdate, userDelete, userReadById, userReadByEmail, userReadByPhone} = require("../controllers/usercontrollers");
+
+router.post("/user-insert", userInsert);
+router.get("/user-read", userRead);
+router.put("/user-update/:id", userUpdate);
+router.delete("/user-delete/:id", userDelete);
+router.get("/user-read-by-id/:id", userReadById);
+router.get("/user-read-by-email/:email", userReadByEmail);
+router.get("/user-read-by-phone/:phone", userReadByPhone);
+
+module.exports = router;
+
+
+// app.post("/user-insert", userInsert);
+// app.get("/user-read", userRead);
+// app.put("/user-update/:id", userUpdate);
+// app.delete("/user-delete/:id", userDelete);
+// app.get("/user-read-by-id/:id", userReadById);
+// app.get("/user-read-by-email/:email", userReadByEmail);
+// app.get("/user-read-by-phone/:phone", userReadByPhone);
+
+// module.exports = router;
