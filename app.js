f972a7adaaefc6e3f007599d408b866d4c15325f
itsm22051@gmail.com <himalpandey2002@gmail.com>
2025-07-06 13:59:34 +0545

    updated

--- app.js
+++ app.js
@@ -10,23 +10,23 @@ connectDB();           // Connect MongoDB
 
 app.use(express.json());
 
-app.use("/api/users", router);
-app.use("/api/users", router);
+app.use("/api/users", router);      // http://localhost:3000/api/users/user-insert
+// app.use("/api/users", router);
 
 app.get("/student-read", (req, res) => {
   res.send("Reading");
 });
 
-app.post("/student-insert", (req, res) => {
+app.post("/student-insert", (req, res) => {     // http://localhost:3000/student-insert
   res.send("Insertion");
 });
 
-app.put("/student-update/:id?", (req, res) => {
+app.put("/student-update/:id", (req, res) => {
   res.send("Update");
 });
 
-app.delete("/student-delete/:id?", (req, res) => {
+app.delete("/student-delete/:id", (req, res) => {
   res.send("Deletion");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
